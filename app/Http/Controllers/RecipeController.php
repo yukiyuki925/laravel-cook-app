93abb32f57f0b7845922183398f172fa8e616ef8
yukiyuki925 <yuki.work1999@gmail.com>
2024-03-13 15:08:51 +0900

    homeビューのデータを取得™

--- app/Http/Controllers/RecipeController.php
+++ app/Http/Controllers/RecipeController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Recipe;
 use Illuminate\Http\Request;
 
 class RecipeController extends Controller
@@ -9,9 +10,32 @@ class RecipeController extends Controller
     /**
      * Display a listing of the resource.
      */
+
+    public function home()
+    {
+      // recipesテーブルから取ってくる
+      $recipes = Recipe::select('recipes.id', 'recipes.title', 'recipes.description', 'recipes.created_at', 'recipes.image', 'users.name')
+      ->join('users', 'users.id', '=', 'recipes.user_id')
+      ->orderBy('recipes.created_at', 'desc')
+      ->limit(3)
+      ->get();
+      // dd($recipes);
+
+      // 人気のレシピを取ってくる
+      $popular = Recipe::select('recipes.id', 'recipes.title', 'recipes.description', 'recipes.created_at', 'recipes.image', 'recipes.views', 'users.name')
+      ->join('users', 'users.id', '=', 'recipes.user_id')
+      ->orderBy('recipes.views', 'desc')
+      ->limit(2)
+      ->get();
+      dd($popular);
+
+      
+      return view('home', compact('recipes','popular'));
+    }
+    
     public function index()
     {
-      return view('home');
+
     }
 
     /**
